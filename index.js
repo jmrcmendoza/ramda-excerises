526beb1d2e939272eed036057813fba64719750f
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-04 18:43:05 +0800

    Added doller average

--- index.js
+++ index.js
@@ -5,46 +5,39 @@ const R = require('ramda');
 
 // // console.log(yellGreeting('James', 'Bond'))
 
-
-
-let cars = [ {
-  name: 'Aston Martin One-77',
-  horsepower: 750,
-  dollarValue: 1850000,
-  inStock: true,
-}, {
-  name: 'Aston Martin One-100',
-  horsepower: 800,
-  dollarValue: 1900000,
-  inStock: false,
-},
-{
-  name: 'Aston Martin One-110',
-  horsepower: 770,
-  dollarValue: 1700000,
-  inStock: false,
-}
-]
-
-
+let cars = [
+  {
+    name: 'Aston Martin One-77',
+    horsepower: 750,
+    dollarValue: 1850000,
+    inStock: true,
+  },
+  {
+    name: 'Aston Martin One-100',
+    horsepower: 800,
+    dollarValue: 1900000,
+    inStock: false,
+  },
+  {
+    name: 'Aston Martin One-110',
+    horsepower: 770,
+    dollarValue: 1700000,
+    inStock: false,
+  },
+];
 
 //Last In Stock
-const isLastInStock  = R.compose(R.prop('inStock'),R.last)(cars)
+const isLastInStock = R.compose(R.prop('inStock'), R.last)(cars);
 
-
+//Dollar Average
+const averageDollarValue = R.compose(
+  R.converge(R.divide, [R.sum, R.length]),
+  R.pluck('dollarValue'),
+)(cars);
 
 //Fastest Car
 const fastestCar = (cars) => {
-
-  const fastest = R.compose(R.last,R.sortBy(R.prop('horsepower')))(cars)
+  const fastest = R.compose(R.last, R.sortBy(R.prop('horsepower')))(cars);
 
   return R.concat(fastest.name, ' is the fastest');
 };
-
-
-
-
-
-
-
-
