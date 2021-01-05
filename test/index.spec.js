132a86c3c7555532200f991e64c27661743aef22
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-05 09:44:49 +0800

    test: updated index test
    added cars average dollar value

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
-import { isLastInStock } from '..';
+import { isLastInStock, averageDollarValue } from '..';
 
 describe('RamdaJS - exercise', () => {
   it('should return last car stock status', async function () {
     expect(isLastInStock).to.equal(false);
+  });
+
+  it('should return the cars average dollar value', () => {
+    expect(averageDollarValue).to.equal(1816666.6666666666666666666666667);
   });
 });
