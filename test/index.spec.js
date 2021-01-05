d5026935e556a359b420a3a1afc0f302e894ced1
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-05 11:20:53 +0800

    refactor: added data inside assertions

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,27 +2,58 @@ import { expect } from 'chai';
 import { isLastInStock, averageDollarValue, fastestCar, totalPayout } from '..';
 
 describe('RamdaJS - exercises', () => {
+  const cars = [
+    {
+      name: 'Aston Martin One-77',
+      horsepower: 750,
+      dollarValue: 1850000,
+      inStock: true,
+    },
+    {
+      name: 'Aston Martin One-100',
+      horsepower: 800,
+      dollarValue: 1900000,
+      inStock: false,
+    },
+    {
+      name: 'Aston Martin One-110',
+      horsepower: 770,
+      dollarValue: 1700000,
+      inStock: true,
+    },
+    {
+      name: 'Aston Martin One-50',
+      horsepower: 700,
+      dollarValue: 1650000,
+      inStock: false,
+    },
+  ];
+
   describe('isLastInStock', () => {
     it('should return last car stock status', async function () {
-      expect(isLastInStock).to.equal(false);
+      expect(isLastInStock(cars)).to.equal(false);
     });
   });
 
   describe('averageDollarValue', () => {
     it('should return the cars average dollar value', () => {
-      expect(averageDollarValue).to.equal(1816666.6666666666666666666666667);
+      expect(averageDollarValue(cars)).to.equal(
+        1816666.6666666666666666666666667,
+      );
     });
   });
 
   describe('fastestCar', () => {
     it('should retrieve the fastest car', () => {
-      expect(fastestCar).to.equal('Aston Martin One-100 is the fastest');
+      expect(fastestCar(cars)).to.equal('Aston Martin One-100 is the fastest');
     });
   });
 
   describe('totalPayout', () => {
+    const data = [{ acc_1: 1 }, { acc_1: 2 }, { acc_2: 3 }];
+
     it('should return the accounts total payout', () => {
-      expect(totalPayout).to.deep.equal([{ acc_1: 3 }, { acc_2: 3 }]);
+      expect(totalPayout(data)).to.deep.equal([{ acc_1: 3 }, { acc_2: 3 }]);
     });
   });
 });
