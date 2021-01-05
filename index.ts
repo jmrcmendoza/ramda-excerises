b6f07bd8662b1b3b4ba0501be5f5ca1a908a115a
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-05 13:25:49 +0800

    feat: changed index to typescript

--- index.ts
+++ index.ts
@@ -1,28 +1,21 @@
 import R from 'ramda';
 
-const isLastInStock = R.compose(R.prop('inStock'), R.last);
+export const isLastInStock = R.compose(R.prop('inStock'), R.last);
 
-const averageDollarValue = R.compose(
+export const averageDollarValue = R.compose(
   R.converge(R.divide, [R.sum, R.length]),
   R.pluck('dollarValue'),
 );
 
-const fastestCar = R.compose(
+export const fastestCar = R.compose(
   R.flip(R.concat)(' is the fastest'),
   R.prop('name'),
   R.last,
   R.sortBy(R.prop('horsepower')),
 );
 
-const totalPayout = R.compose(
+export const totalPayout = R.compose(
   R.values,
   R.map(R.reduce(R.mergeWith(R.add), 0)),
   R.groupBy(R.keys),
 );
-
-module.exports = {
-  isLastInStock,
-  averageDollarValue,
-  fastestCar,
-  totalPayout,
-};
