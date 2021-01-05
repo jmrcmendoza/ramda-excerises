481e0426d048b4931fcd5aeb3c70c4b7f8564810
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-05 10:03:53 +0800

    test: categorized the tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,28 @@
 import { expect } from 'chai';
 import { isLastInStock, averageDollarValue, fastestCar, totalPayout } from '..';
 
-describe('RamdaJS - exercise', () => {
-  it('should return last car stock status', async function () {
-    expect(isLastInStock).to.equal(false);
+describe('RamdaJS - exercises', () => {
+  describe('isLastInStock', () => {
+    it('should return last car stock status', async function () {
+      expect(isLastInStock).to.equal(false);
+    });
   });
 
-  it('should return the cars average dollar value', () => {
-    expect(averageDollarValue).to.equal(1816666.6666666666666666666666667);
+  describe('averageDollarValue', () => {
+    it('should return the cars average dollar value', () => {
+      expect(averageDollarValue).to.equal(1816666.6666666666666666666666667);
+    });
   });
 
-  it('should retrieve the fastest car', () => {
-    expect(fastestCar).to.equal('Aston Martin One-100 is the fastest');
+  describe('fastestCar', () => {
+    it('should retrieve the fastest car', () => {
+      expect(fastestCar).to.equal('Aston Martin One-100 is the fastest');
+    });
   });
 
-  it('should return the accounts total payout', () => {
-    expect(totalPayout).to.deep.equal([{ acc_1: 3 }, { acc_2: 3 }]);
+  describe('totalPayout', () => {
+    it('should return the accounts total payout', () => {
+      expect(totalPayout).to.deep.equal([{ acc_1: 3 }, { acc_2: 3 }]);
+    });
   });
 });
