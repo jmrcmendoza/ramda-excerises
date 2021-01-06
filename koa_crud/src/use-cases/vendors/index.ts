0246cb376d8a123f86c0443b7a5f9b2c73ab911f
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-06 10:19:08 +0800

    feat: added vendor use cases

---
+++ koa_crud/src/use-cases/vendors/index.ts
@@ -0,0 +1,5 @@
+import makeListVendors from './list_vendors';
+
+const listVendors = makeListVendors();
+
+export default listVendors;
---
+++ koa_crud/src/use-cases/vendors/list_vendors.ts
@@ -0,0 +1,9 @@
+export default function makeListVendors() {
+  return async function listVendors(): Promise<any> {
+    const data = {
+      message: 'Successfull..',
+    };
+
+    return data;
+  };
+}
