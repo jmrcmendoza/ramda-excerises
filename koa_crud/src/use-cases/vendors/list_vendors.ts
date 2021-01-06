b7c0f75b3b1eca85002a865a4c198205a81d4654
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-06 12:10:14 +0800

    feat: updated list vendor use case

--- koa_crud/src/use-cases/vendors/list_vendors.ts
+++ koa_crud/src/use-cases/vendors/list_vendors.ts
@@ -1,7 +1,10 @@
-export default function makeListVendors() {
+export default function makeListVendors({ vendor }) {
   return async function listVendors(): Promise<any> {
+    const allVendors = await vendor.find({});
+
     const data = {
       message: 'Successfull..',
+      vendors: allVendors,
     };
 
     return data;
