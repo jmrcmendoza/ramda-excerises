75f2ba3cec0c31a46cc63cf162101c4edcaeb806
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-06 15:44:25 +0800

    feat: added select one vendor route

--- koa_crud/src/routes/vendors/index.ts
+++ koa_crud/src/routes/vendors/index.ts
@@ -2,6 +2,7 @@ import Router from 'koa-router';
 
 import {
   getVendors,
+  getOneVendor,
   postVendor,
   putVendor,
   delVendor,
@@ -12,6 +13,7 @@ import { serialize } from '../../serialize';
 const router = new Router();
 
 router.get('/api/vendors', serialize(getVendors));
+router.get('/api/vendors/:_id', serialize(getOneVendor));
 router.post('/api/vendors/', serialize(postVendor));
 router.put('/api/vendors/:_id', serialize(putVendor));
 router.delete('/api/vendors/:_id', serialize(delVendor));
