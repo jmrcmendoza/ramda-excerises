88acb1c01b2e4c98ef04fc8186a4fde096a49f84
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-06 14:37:17 +0800

    feat: added update vendor in routes

--- koa_crud/src/routes/vendors/index.ts
+++ koa_crud/src/routes/vendors/index.ts
@@ -1,6 +1,6 @@
 import Router from 'koa-router';
 
-import { getVendors, postVendor } from '../../controllers/vendors';
+import { getVendors, postVendor, putVendor } from '../../controllers/vendors';
 
 import { serialize } from '../../serialize';
 
@@ -8,5 +8,6 @@ const router = new Router();
 
 router.get('/api/vendors', serialize(getVendors));
 router.post('/api/vendors/create', serialize(postVendor));
+router.put('/api/vendors/update/:_id', serialize(putVendor));
 
 export default router;
