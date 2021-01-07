58ab9691f7317035e8f396f6164ced46f79f34e6
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 13:37:08 +0800

    test: updated list vendors
    assign requests to a variable

--- koa_crud/test/vendors/vendors.spec.ts
+++ koa_crud/test/vendors/vendors.spec.ts
@@ -64,26 +64,25 @@ describe('Vendors', () => {
 
   describe('List vendors', () => {
     it('should return all vendors', async function () {
-      await chai
+      const response = await chai
         .request('http://localhost:3000')
-        .get('/api/vendors')
-        .then((res) => {
-          expect(res.body.vendors).to.exist;
-          expect(res.body.vendors)
-            .to.be.an('array')
-            .that.has.length.greaterThan(0);
-        });
+        .get('/api/vendors');
+
+      expect(response.body.vendors).to.exist;
+      expect(response.body.vendors)
+        .to.be.an('array')
+        .that.has.length.greaterThan(0);
     });
 
     it('should return one vendor', async function () {
       const id = '5ff679a79de0a13d142d9fce';
 
-      await chai
+      const response = await chai
         .request('http://localhost:3000')
-        .get(`/api/vendors/${id}`)
-        .then((res) => {
-          expect(res.body.vendor).to.exist;
-        });
+        .get(`/api/vendors/${id}`);
+
+      expect(response.body.vendor).to.exist;
+      expect(response.body.vendor).to.be.an('object');
     });
   });
 
