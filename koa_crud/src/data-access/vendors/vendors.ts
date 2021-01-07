36fd332fdc75c34c746ca4a13b16f39026721c47
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 13:28:08 +0800

    refactor: remove function initializations

--- koa_crud/src/data-access/vendors/vendors.ts
+++ koa_crud/src/data-access/vendors/vendors.ts
@@ -5,35 +5,25 @@ export default function vendorsQueries({
 }: {
   vendors: typeof VendorModel;
 }): any {
-  async function listVendors() {
-    return vendors.find({}).lean();
-  }
-
-  async function selectOneVendor(id: string) {
-    return vendors.findById(id).lean();
-  }
-
-  async function createVendor(vendorInfo: any) {
-    return vendors.create(vendorInfo);
-  }
-
-  async function updateVendor(id: string, vendorInfo: any) {
-    return vendors.findOneAndUpdate(
-      { _id: id },
-      { ...vendorInfo },
-      { useFindAndModify: false },
-    );
-  }
-
-  async function deleteVendor(id: string) {
-    return vendors.deleteOne({ _id: id });
-  }
-
   return Object.freeze({
-    listVendors,
-    selectOneVendor,
-    createVendor,
-    updateVendor,
-    deleteVendor,
+    listVendors() {
+      return vendors.find({}).lean();
+    },
+    selectOneVendor(id: string) {
+      return vendors.findById(id).lean();
+    },
+    createVendor(vendorInfo: any) {
+      return vendors.create(vendorInfo);
+    },
+    updateVendor(id: string, vendorInfo: any) {
+      return vendors.findOneAndUpdate(
+        { _id: id },
+        { ...vendorInfo },
+        { useFindAndModify: false },
+      );
+    },
+    deleteVendor(id: string) {
+      return vendors.deleteOne({ _id: id });
+    },
   });
 }
