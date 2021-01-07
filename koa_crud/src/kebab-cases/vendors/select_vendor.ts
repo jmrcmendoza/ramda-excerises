10b284c1e263d0a0909934783464f03e2e1a78b8
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 10:32:52 +0800

    feat: included id validation

--- koa_crud/src/kebab-cases/vendors/select_vendor.ts
+++ koa_crud/src/kebab-cases/vendors/select_vendor.ts
@@ -1,5 +1,9 @@
 export default function makeSelectVendor(vendorsDB: Record<string, any>) {
   return async function selectVendor(id: string): Promise<any> {
+    if (!id) {
+      throw new Error('ID must be provided.');
+    }
+
     const vendor = await vendorsDB.selectOneVendor(id);
 
     const data = {
