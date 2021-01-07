3f0cdbf4aa313ba79cc1602b86259c95a7e6b7ac
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 09:35:08 +0800

    refactor: updated types

--- koa_crud/src/use-cases/vendors/select_vendor.ts
+++ koa_crud/src/use-cases/vendors/select_vendor.ts
@@ -1,4 +1,4 @@
-export default function makeSelectVendor(vendorsDB) {
+export default function makeSelectVendor(vendorsDB: Record<string, any>) {
   return async function selectVendor(_id: string): Promise<any> {
     const vendor = await vendorsDB.selectOneVendor(_id);
 
