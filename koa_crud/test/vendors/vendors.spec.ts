048640a2ea1daa5fdbfaefc43dc05361c70461ad
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 11:07:32 +0800

    test: added vendor test
    list vendors

---
+++ koa_crud/test/vendors/vendors.spec.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-expressions */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Koa CRUD', () => {
+  describe('Vendors', () => {
+    describe('List vendors', () => {
+      it('should return all vendors', async function () {
+        await chai
+          .request('http://localhost:3000')
+          .get('/api/vendors')
+          .then((res) => {
+            expect(res.body.vendors).to.exist;
+            expect(res.body.vendors)
+              .to.be.an('array')
+              .that.has.length.greaterThan(0);
+          });
+      });
+    });
+  });
+});
