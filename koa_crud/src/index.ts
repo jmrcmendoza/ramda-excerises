e754036ce84ce77fc9cfdd5ea10b823ea1bf539a
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 09:01:32 +0800

    refactor: moved db initialization to root index

--- koa_crud/src/index.ts
+++ koa_crud/src/index.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
 
 import vendorsRoutes from './routes/vendors';
 
@@ -7,6 +8,20 @@ const app = new Koa();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser());
+
+const uri = 'mongodb://localhost:27017/local';
+
+mongoose.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err: any) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('Successfully connected to MongoDB');
+    }
+  },
+);
 
 app.use(vendorsRoutes.routes());
 
