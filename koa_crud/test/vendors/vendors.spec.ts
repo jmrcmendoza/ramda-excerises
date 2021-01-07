ec559d95f21f75abcb488733109e18dc50125706
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 11:59:55 +0800

    test: updated vendor test
    added test for edit vendor

--- koa_crud/test/vendors/vendors.spec.ts
+++ koa_crud/test/vendors/vendors.spec.ts
@@ -90,5 +90,56 @@ describe('Koa CRUD', () => {
           });
       });
     });
+
+    describe('Edit vendor', () => {
+      it('should return error for null vendor name', async function () {
+        const data = {
+          name: undefined,
+          type: 'SEEMLESS',
+        };
+
+        await chai
+          .request('http://localhost:3000')
+          .put(`/api/vendors/${createdVendorId}`)
+          .send(data)
+          .then((res) => {
+            expect(res.status).to.equal(400);
+            expect(res.body.errorMsg).to.equal('Vendor name must be provided.');
+          });
+      });
+
+      it('should return error for null vendor type', async function () {
+        const data = {
+          name: 'Vendor 2',
+          type: undefined,
+        };
+
+        await chai
+          .request('http://localhost:3000')
+          .put(`/api/vendors/${createdVendorId}`)
+          .send(data)
+          .then((res) => {
+            expect(res.status).to.equal(400);
+            expect(res.body.errorMsg).to.equal('Vendor type must be provided.');
+          });
+      });
+
+      it('should update vendor type', async function () {
+        const data = {
+          name: 'Vendor 2',
+          type: 'TRANSFER',
+        };
+
+        await chai
+          .request('http://localhost:3000')
+          .put(`/api/vendors/${createdVendorId}`)
+          .send(data)
+          .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body.result).to.exist;
+            expect(res.body.result).to.be.an('object');
+          });
+      });
+    });
   });
 });
