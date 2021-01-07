ace3007eb20c8f81981e42e7956a4c79c8a15b1f
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-07 09:34:29 +0800

    refactor: updated types

--- koa_crud/src/use-cases/vendors/add_vendor.ts
+++ koa_crud/src/use-cases/vendors/add_vendor.ts
@@ -1,7 +1,8 @@
+import { VendorDocument } from '../../models/vendor';
 import { makeVendor } from '../../entities/vendors';
 
-export default function makeInsertVendor(vendorsDB) {
-  return async function insertVendor(vendorInfo): Promise<any> {
+export default function makeInsertVendor(vendorsDB: Record<string, any>) {
+  return async function insertVendor(vendorInfo: VendorDocument): Promise<any> {
     await makeVendor(vendorInfo);
 
     const createVendor = await vendorsDB.createVendor(vendorInfo);
