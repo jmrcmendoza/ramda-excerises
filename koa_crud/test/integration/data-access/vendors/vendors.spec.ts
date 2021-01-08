3372faa55a0ca8b31648a3b3a3ee6fee0e3d9caa
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-08 14:12:52 +0800

    test: added data access integration test
    added create scenarios

---
+++ koa_crud/test/integration/data-access/vendors/vendors.spec.ts
@@ -0,0 +1,60 @@
+/* eslint-disable no-unused-expressions */
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import chaiAsPromised from 'chai-as-promised';
+import server from '../../../../src';
+import vendorsDB from '../../../../src/data-access/vendors';
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+
+before(function () {
+  this.request = () => chai.request(server);
+});
+
+describe('Vendor Data Access', () => {
+  describe('Create a Vendor', async () => {
+    context('Given values are incorrect', () => {
+      it('should throw a validation error for empty vendor name', async () => {
+        const data = {
+          name: '',
+          type: 'SEAMLESS',
+        };
+
+        await expect(vendorsDB.createVendor(data)).to.eventually.rejected;
+      });
+
+      it('should throw a validation error for empty vendor type', async () => {
+        const data = {
+          name: 'Data Access Vendor 1',
+          type: '',
+        };
+
+        await expect(vendorsDB.createVendor(data)).to.eventually.rejected;
+      });
+
+      it('should throw a validation error for invalid vendor type', async () => {
+        const data = {
+          name: 'Data Access Vendor 2',
+          type: 'Test Type',
+        };
+
+        await expect(vendorsDB.createVendor(data)).to.eventually.rejected;
+      });
+    });
+
+    context('Given the values are correct', () => {
+      it('should insert vendor and return the values', async () => {
+        const data = {
+          name: 'Data Access Vendor 3',
+          type: 'TRANSFER',
+        };
+
+        const result = await vendorsDB.createVendor(data);
+
+        expect(result).to.exist;
+        expect(result).to.be.an('object');
+      });
+    });
+  });
+});
