aea2b26119a305a7ca6279b1112d233896421cb5
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-12 12:05:26 +0800

    feat: updated list vendors return value

--- koa_crud/src/use-cases/vendors/list-vendors.ts
+++ koa_crud/src/use-cases/vendors/list-vendors.ts
@@ -2,11 +2,6 @@ import { VendorQueries } from '../../data-access/vendors/vendors';
 
 export default function makeListVendors(vendorsDB: VendorQueries) {
   return async function listVendors(): Promise<any> {
-    const allVendors = await vendorsDB.listVendors();
-
-    return {
-      message: 'Successful',
-      vendors: allVendors,
-    };
+    return vendorsDB.listVendors();
   };
 }
