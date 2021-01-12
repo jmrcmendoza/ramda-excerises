43cf6f44c8304ef9dd3d0fefd93d3e7712511ad6
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-12 14:55:32 +0800

    test: updated vendor data access
    updated delete test data

--- koa_crud/test/integration/data-access/vendors/vendors.spec.ts
+++ koa_crud/test/integration/data-access/vendors/vendors.spec.ts
@@ -106,9 +106,9 @@ describe('Vendor Data Access', () => {
     it('should delete created vendor', async () => {
       const vendors = await vendorsDB.listVendors();
 
-      const result = await vendorsDB.deleteVendor(
-        R.compose(R.prop('_id'), R.last)(vendors),
-      );
+      const lastVendorId = R.compose(R.prop('_id'), R.last)(vendors);
+
+      const result = await vendorsDB.deleteVendor(lastVendorId);
 
       expect(result).to.exist;
       expect(result).to.be.true;
