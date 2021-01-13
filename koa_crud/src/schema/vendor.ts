63dff12aa261582093f93e3353228404c446b69f
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 10:52:03 +0800

    feat: move resolvers into one file

--- koa_crud/src/schema/vendor.ts
+++ koa_crud/src/schema/vendor.ts
@@ -1,17 +1,5 @@
 import { gql } from 'apollo-server-koa';
-import { VendorDocument } from '../models/vendor';
 
-import {
-  insertVendor,
-  listVendors,
-  selectVendor,
-  updateVendor,
-  deleteVendor,
-} from '../use-cases/vendors';
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 export const typeDefs = gql`
   enum VendorType {
     SEAMLESS
@@ -26,7 +14,7 @@ export const typeDefs = gql`
     updatedAt: String
   }
 
-  type Query {
+  extend type Query {
     vendors: [Vendor]
     vendor(id: ID!): Vendor
   }
@@ -41,49 +29,9 @@ export const typeDefs = gql`
     type: VendorType
   }
 
-  type Mutation {
+  extend type Mutation {
     createVendor(input: CreateVendorInput!): Boolean
     updateVendor(id: ID!, input: UpdateVendorInput!): Boolean
     deleteVendor(id: ID!): Boolean
   }
 `;
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-export const resolvers = {
-  Query: {
-    vendors: async (): Promise<any> => listVendors(),
-    vendor: async (
-      _obj: any,
-      vendor: { id: string },
-    ): Promise<Record<string, any>> => selectVendor(vendor.id),
-  },
-
-  Vendor: {
-    type(obj: { type: string }): string {
-      if (obj.type === 'SEAMLESS') return 'SEAMLESS';
-      if (obj.type === 'TRANSFER') return 'TRANSFER';
-      return 'SEAMLESS';
-    },
-    // name: (obj: any) => `Vendor - ${obj.name}`,
-  },
-
-  Mutation: {
-    createVendor: async (
-      _obj: any,
-      args: {
-        input: VendorDocument;
-      },
-    ): Promise<Record<string, any>> => insertVendor(args.input),
-    updateVendor: async (
-      _obj: any,
-      args: {
-        id: string;
-        input: VendorDocument;
-      },
-    ): Promise<boolean> => updateVendor(args.id, args.input),
-
-    deleteVendor: async (_obj: any, args: { id: string }): Promise<boolean> =>
-      deleteVendor(args.id),
-  },
-};
