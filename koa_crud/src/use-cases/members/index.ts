aae72fc128cf5763c7a96deb52d280685d997ced
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 09:10:28 +0800

    feat: added update member use case

--- koa_crud/src/use-cases/members/index.ts
+++ koa_crud/src/use-cases/members/index.ts
@@ -1,9 +1,11 @@
 import makeListMembers from './list-members';
 import makeSelectMember from './select-vendor';
 import makeInsertMember from './add-member';
+import makeUpdateMember from './edit-member';
 
 import membersDB from '../../data-access/members';
 
 export const listMembers = makeListMembers(membersDB);
 export const selectMember = makeSelectMember(membersDB);
 export const insertMember = makeInsertMember(membersDB);
+export const updateMember = makeUpdateMember(membersDB);
