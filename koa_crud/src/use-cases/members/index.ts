c327272d3619c8b04ff61d9052585914b909fe7f
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 09:18:38 +0800

    feat: added delete use case

--- koa_crud/src/use-cases/members/index.ts
+++ koa_crud/src/use-cases/members/index.ts
@@ -2,6 +2,7 @@ import makeListMembers from './list-members';
 import makeSelectMember from './select-vendor';
 import makeInsertMember from './add-member';
 import makeUpdateMember from './edit-member';
+import makeDeleteMember from './delete-member';
 
 import membersDB from '../../data-access/members';
 
@@ -9,3 +10,4 @@ export const listMembers = makeListMembers(membersDB);
 export const selectMember = makeSelectMember(membersDB);
 export const insertMember = makeInsertMember(membersDB);
 export const updateMember = makeUpdateMember(membersDB);
+export const deleteMember = makeDeleteMember(membersDB);
