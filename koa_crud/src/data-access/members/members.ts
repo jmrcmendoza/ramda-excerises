8671d5593471d030aceccc855d7d301fc49b4cce
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 08:48:09 +0800

    feat: added member data access

---
+++ koa_crud/src/data-access/members/members.ts
@@ -0,0 +1,41 @@
+import MemberModel, { MemberDocument } from '../../models/member';
+
+export type MemberQueries = {
+  listMembers: () => Promise<MemberDocument>;
+  selectOneMember: (id: string) => Promise<MemberDocument>;
+  createMember: (document: MemberDocument) => Promise<boolean>;
+  updateMember: (id: string, document: MemberDocument) => Promise<boolean>;
+  deleteMember: (id: string) => Promise<boolean>;
+};
+
+export default function ({
+  member,
+}: {
+  member: typeof MemberModel;
+}): MemberQueries {
+  return Object.freeze({
+    listMembers() {
+      return member.find({}).lean();
+    },
+    selectOneMember(id: string) {
+      return member.findById(id).lean();
+    },
+    async createMember(vendorInfo: MemberDocument) {
+      const result = await member.create(vendorInfo);
+
+      return !!result;
+    },
+    async updateMember(id: string, vendorInfo: MemberDocument) {
+      const result = await member.findByIdAndUpdate(id, vendorInfo, {
+        useFindAndModify: false,
+      });
+
+      return !!result;
+    },
+    async deleteMember(id: string) {
+      const result = await member.findByIdAndDelete(id);
+
+      return !!result;
+    },
+  });
+}
