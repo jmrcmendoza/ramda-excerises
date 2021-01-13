a53788cfa42b2f85995a027ec7347dd89ae58839
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 14:05:54 +0800

    test: added member controller test
    added create scenarios

---
+++ koa_crud/test/integration/controllers/members/members.spec.ts
@@ -0,0 +1,106 @@
+/* eslint-disable no-unused-expressions */
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import chaiAsPromised from 'chai-as-promised';
+import Chance from 'chance';
+import R from 'ramda';
+import server from '../../../../src';
+import { getMembers, postMember } from '../../../../src/controllers/members';
+
+const chance = new Chance();
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+
+describe('Member Controller', () => {
+  before(function () {
+    this.request = () => chai.request(server);
+  });
+
+  describe('Create Member', () => {
+    context('Given incorrect values', () => {
+      it('should return 400 status code for empty username', async () => {
+        const data = {
+          body: {
+            username: '',
+            password: chance.string({ length: 5 }),
+            realName: chance.name(),
+          },
+          headers: {
+            'Content-Type': null,
+            Referer: null,
+            'User-Agent': null,
+          },
+        };
+
+        const result = await postMember(data);
+
+        expect(result).to.have.property('status', 400);
+      });
+
+      it('should return 400 status code for empty password', async () => {
+        const data = {
+          body: {
+            username: chance.last(),
+            password: '',
+            realName: chance.name(),
+          },
+          headers: {
+            'Content-Type': null,
+            Referer: null,
+            'User-Agent': null,
+          },
+        };
+
+        const result = await postMember(data);
+
+        expect(result).to.have.property('status', 400);
+      });
+    });
+
+    context('Given correct values', () => {
+      it('should insert member and return 200 status code', async () => {
+        const data = {
+          body: {
+            username: chance.last(),
+            password: chance.string({ length: 5 }),
+            realName: chance.name(),
+          },
+          headers: {
+            'Content-Type': null,
+            Referer: null,
+            'User-Agent': null,
+          },
+        };
+
+        const result = await postMember(data);
+
+        expect(result).to.have.property('status', 201);
+        expect(result.body).to.be.true;
+      });
+
+      it('should return 400 status code for duplicate username', async () => {
+        const members = await getMembers();
+
+        const lastMember = R.last(members.body);
+
+        const data = {
+          body: {
+            username: lastMember.username,
+            password: chance.string({ length: 5 }),
+            realName: chance.name(),
+          },
+          headers: {
+            'Content-Type': null,
+            Referer: null,
+            'User-Agent': null,
+          },
+        };
+
+        const result = await postMember(data);
+
+        expect(result).to.have.property('status', 400);
+      });
+    });
+  });
+});
