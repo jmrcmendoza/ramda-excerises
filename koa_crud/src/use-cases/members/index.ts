01139846f3d7649b521d043fa89beb349c7a9e02
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 08:55:43 +0800

    feat: added select one member use case

--- koa_crud/src/use-cases/members/index.ts
+++ koa_crud/src/use-cases/members/index.ts
@@ -1,5 +1,7 @@
 import makeListMembers from './list-members';
+import makeSelectMember from './select-vendor';
 
 import membersDB from '../../data-access/members';
 
 export const listMembers = makeListMembers(membersDB);
+export const selectMember = makeSelectMember(membersDB);
