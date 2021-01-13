dd041fa6b93bae7756020bd123303b6be89efa71
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 09:12:12 +0800

    feat: added update member route

--- koa_crud/src/routes/members/index.ts
+++ koa_crud/src/routes/members/index.ts
@@ -4,6 +4,7 @@ import {
   getMembers,
   getOneMember,
   postMember,
+  putMember,
 } from '../../controllers/members';
 
 import { serialize } from '../../serialize';
@@ -13,5 +14,6 @@ const router = new Router();
 router.get('/api/members', serialize(getMembers));
 router.get('/api/members/:id', serialize(getOneMember));
 router.post('/api/members', serialize(postMember));
+router.put('/api/members/:id', serialize(putMember));
 
 export default router;
