8c495eb7b2b350106cd71062757ac3b893424ed2
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-13 09:11:01 +0800

    feat: updated member controller
    added update member
    updated insert member data initialization

--- koa_crud/src/controllers/members/edit-member.ts
+++ koa_crud/src/controllers/members/edit-member.ts
@@ -1,23 +1,26 @@
 import { Context } from 'koa';
 import { MemberDocument } from '../../models/member';
 
-export default function insertMemberController({
-  insertMember,
+export default function updateMemberController({
+  updateMember,
 }: {
-  insertMember: (document: MemberDocument) => Promise<boolean>;
+  updateMember: (id: string, document: MemberDocument) => Promise<boolean>;
 }) {
   return async function postInsertMember(
     httpRequest: Context,
   ): Promise<Record<string, any>> {
     try {
+      const { id } = httpRequest.params;
+
       const memberInfo = httpRequest.body;
-      const result = await insertMember(memberInfo);
+
+      const result = await updateMember(id, memberInfo);
 
       return {
         headers: {
           'Content-Type': 'application/json',
         },
-        status: 201,
+        status: 200,
         body: result,
       };
     } catch (e) {
--- koa_crud/src/controllers/members/index.ts
+++ koa_crud/src/controllers/members/index.ts
@@ -2,12 +2,15 @@ import {
   listMembers,
   selectMember,
   insertMember,
+  updateMember,
 } from '../../use-cases/members';
 
 import listMembersController from './list-members';
 import selectMemberController from './select-vendor';
 import insertMemberController from './add-member';
+import updateMemberController from './edit-member';
 
 export const getMembers = listMembersController({ listMembers });
 export const getOneMember = selectMemberController({ selectMember });
 export const postMember = insertMemberController({ insertMember });
+export const putMember = updateMemberController({ updateMember });
