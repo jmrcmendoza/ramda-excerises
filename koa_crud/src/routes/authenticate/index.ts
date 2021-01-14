a6309c77294b3bedb090c4f3f8ee2584760b6a63
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-14 15:47:05 +0800

    feat: added authenticate route

---
+++ koa_crud/src/routes/authenticate/index.ts
@@ -0,0 +1,11 @@
+import Router from 'koa-router';
+
+import { authMember } from '../../controllers/authenticate';
+
+import { serialize } from '../../serialize';
+
+const router = new Router();
+
+router.post('/api/authenticate', serialize(authMember));
+
+export default router;
