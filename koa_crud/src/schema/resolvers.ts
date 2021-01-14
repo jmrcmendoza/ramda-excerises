9f27ce33c036a431746504a9a509abc299ae76a4
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-14 15:48:33 +0800

    feat: updated resolvers
    added authenticate resolver
    implemented authentication

--- koa_crud/src/schema/resolvers.ts
+++ koa_crud/src/schema/resolvers.ts
@@ -1,4 +1,5 @@
 import { MemberDocument } from '../models/member';
+import { Context } from 'koa';
 
 import {
   insertMember,
@@ -18,18 +19,46 @@ import {
   deleteVendor,
 } from '../use-cases/vendors';
 
+import { authenticateMember } from '../use-cases/authenticate';
+
 export default {
   Query: {
-    vendors: async (): Promise<any> => listVendors(),
+    vendors: async (_obj: any, _arg: any, ctx: Context): Promise<any> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return listVendors();
+    },
     vendor: async (
       _obj: any,
       vendor: { id: string },
-    ): Promise<Record<string, any>> => selectVendor(vendor.id),
-    members: async (): Promise<any> => listMembers(),
+      ctx: Context,
+    ): Promise<Record<string, any>> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return selectVendor(vendor.id);
+    },
+    members: async (_obj: any, _arg: any, ctx: Context): Promise<any> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return listMembers();
+    },
     member: async (
       _obj: any,
       args: { id: string },
-    ): Promise<Record<string, any>> => selectMember(args.id),
+      ctx: Context,
+    ): Promise<Record<string, any>> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return selectMember(args.id);
+    },
   },
 
   Member: {
@@ -53,33 +82,86 @@ export default {
       args: {
         input: MemberDocument;
       },
-    ): Promise<boolean> => insertMember(args.input),
+      ctx: Context,
+    ): Promise<boolean> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return insertMember(args.input);
+    },
     updateMember: async (
       _obj: any,
       args: {
         id: string;
         input: MemberDocument;
       },
-    ): Promise<boolean> => updateMember(args.id, args.input),
+      ctx: Context,
+    ): Promise<boolean> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
 
-    deleteMember: async (_obj: any, args: { id: string }): Promise<boolean> =>
-      deleteMember(args.id),
+      return updateMember(args.id, args.input);
+    },
+
+    deleteMember: async (
+      _obj: any,
+      args: { id: string },
+      ctx: Context,
+    ): Promise<boolean> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return deleteMember(args.id);
+    },
+
+    authenticate: async (
+      _obj: any,
+      args: { input: MemberDocument },
+    ): Promise<{ token: string }> => {
+      return authenticateMember(args.input);
+    },
 
     createVendor: async (
       _obj: any,
       args: {
         input: VendorDocument;
       },
-    ): Promise<Record<string, any>> => insertVendor(args.input),
+      ctx: Context,
+    ): Promise<Record<string, any>> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return insertVendor(args.input);
+    },
     updateVendor: async (
       _obj: any,
       args: {
         id: string;
         input: VendorDocument;
       },
-    ): Promise<boolean> => updateVendor(args.id, args.input),
+      ctx: Context,
+    ): Promise<boolean> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
 
-    deleteVendor: async (_obj: any, args: { id: string }): Promise<boolean> =>
-      deleteVendor(args.id),
+      return updateVendor(args.id, args.input);
+    },
+
+    deleteVendor: async (
+      _obj: any,
+      args: { id: string },
+      ctx: Context,
+    ): Promise<boolean> => {
+      if (!ctx.verified) {
+        throw new Error('Forbidden');
+      }
+
+      return deleteVendor(args.id);
+    },
   },
 };
