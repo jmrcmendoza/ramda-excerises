2e3287e1e69914a74ea5b59f311e9b9fc5b65b85
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 11:27:15 +0800

    feat: updated promo controller
    add update promo

---
+++ koa_crud/src/controllers/promos/edit-promo.ts
@@ -0,0 +1,37 @@
+import { Context } from 'koa';
+import { PromoDocument } from '../../models/promo';
+
+export default function updatePromoController({
+  updatePromo,
+}: {
+  updatePromo: (id: string, document: PromoDocument) => Promise<boolean>;
+}) {
+  return async function postInsertPromo(
+    httpRequest: Context,
+  ): Promise<Record<string, any>> {
+    try {
+      const {
+        params: { id },
+        body: promoInfo,
+      } = httpRequest;
+
+      const result = await updatePromo(id, promoInfo);
+
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        status: 200,
+        body: result,
+      };
+    } catch (e) {
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        status: e.status ? e.status : 400,
+        body: { errorMsg: e.message },
+      };
+    }
+  };
+}
--- koa_crud/src/controllers/promos/index.ts
+++ koa_crud/src/controllers/promos/index.ts
@@ -1,9 +1,16 @@
-import { listPromos, selectPromo, insertPromo } from '../../use-cases/promos';
+import {
+  listPromos,
+  selectPromo,
+  insertPromo,
+  updatePromo,
+} from '../../use-cases/promos';
 
 import listPromosController from './list-promos';
 import selectPromoController from './select-promo';
 import insertPromoController from './add-promo';
+import updatePromoController from './edit-promo';
 
 export const getPromos = listPromosController({ listPromos });
 export const getOnePromo = selectPromoController({ selectPromo });
 export const postPromo = insertPromoController({ insertPromo });
+export const putPromo = updatePromoController({ updatePromo });
