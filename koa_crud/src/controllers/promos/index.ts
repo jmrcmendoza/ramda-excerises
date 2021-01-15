55397ae9cb8234a172af7bb424fc8ec479868e80
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 11:20:32 +0800

    feat: updated promo controller
    add select one promo

--- koa_crud/src/controllers/promos/index.ts
+++ koa_crud/src/controllers/promos/index.ts
@@ -1,7 +1,9 @@
-import { listPromos, insertPromo } from '../../use-cases/promos';
+import { listPromos, selectPromo, insertPromo } from '../../use-cases/promos';
 
 import listPromosController from './list-promos';
+import selectPromoController from './select-promo';
 import insertPromoController from './add-promo';
 
 export const getPromos = listPromosController({ listPromos });
+export const getOnePromo = selectPromoController({ selectPromo });
 export const postPromo = insertPromoController({ insertPromo });
---
+++ koa_crud/src/controllers/promos/select-promo.ts
@@ -0,0 +1,34 @@
+import { Context } from 'koa';
+import { PromoDocument } from '../../models/promo';
+
+export default function selectPromoController({
+  selectPromo,
+}: {
+  selectPromo: (id: string) => Promise<PromoDocument>;
+}) {
+  return async function getOnePromo(
+    httpRequest: Context,
+  ): Promise<Record<string, any>> {
+    try {
+      const { id } = httpRequest.params;
+
+      const result = await selectPromo(id);
+
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        status: 200,
+        body: result,
+      };
+    } catch (e) {
+      return {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        status: e.status ? e.status : 400,
+        body: { errorMsg: e.message },
+      };
+    }
+  };
+}
