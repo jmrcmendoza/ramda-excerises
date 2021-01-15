c6b1c69b6ce6ad9caf1d068da9cc75af168d7423
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 11:20:51 +0800

    feat: updated promo routes
    add select one promo

--- koa_crud/src/routes/promos/index.ts
+++ koa_crud/src/routes/promos/index.ts
@@ -1,12 +1,13 @@
 import Router from 'koa-router';
 
-import { getPromos, postPromo } from '../../controllers/promos';
+import { getPromos, getOnePromo, postPromo } from '../../controllers/promos';
 
 import { serialize } from '../../serialize';
 
 const router = new Router();
 
 router.get('/api/promos', serialize(getPromos));
+router.get('/api/promos/:id', serialize(getOnePromo));
 router.post('/api/promos/', serialize(postPromo));
 
 export default router;
