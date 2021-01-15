4ca22515d7c5feef027102fd7e1cfcdf62a5aff2
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 13:20:35 +0800

    feat: updated promo controller
    added delete promo

--- koa_crud/src/controllers/promos/index.ts
+++ koa_crud/src/controllers/promos/index.ts
@@ -3,14 +3,17 @@ import {
   selectPromo,
   insertPromo,
   updatePromo,
+  deletePromo,
 } from '../../use-cases/promos';
 
 import listPromosController from './list-promos';
 import selectPromoController from './select-promo';
 import insertPromoController from './add-promo';
 import updatePromoController from './edit-promo';
+import deletePromoController from './delete-promo';
 
 export const getPromos = listPromosController({ listPromos });
 export const getOnePromo = selectPromoController({ selectPromo });
 export const postPromo = insertPromoController({ insertPromo });
 export const putPromo = updatePromoController({ updatePromo });
+export const delPromo = deletePromoController({ deletePromo });
