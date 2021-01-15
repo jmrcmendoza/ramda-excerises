278163ebfa930d14263a066c8f5f3331e7cd3ff5
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 11:20:14 +0800

    feat: updated promo use casee
    add select one promo

--- koa_crud/src/use-cases/promos/index.ts
+++ koa_crud/src/use-cases/promos/index.ts
@@ -1,7 +1,9 @@
 import makeListPromos from './list-promos';
+import makeSelectPromo from './select-promo';
 import makeInsertPromo from './add-promo';
 
 import promosDB from '../../data-access/promos';
 
 export const listPromos = makeListPromos(promosDB);
+export const selectPromo = makeSelectPromo(promosDB);
 export const insertPromo = makeInsertPromo(promosDB);
