c7be9af488364ae60163837d21408a712b902ce7
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-15 11:26:52 +0800

    feat: updated promo use case
    add update promo

--- koa_crud/src/use-cases/promos/index.ts
+++ koa_crud/src/use-cases/promos/index.ts
@@ -1,9 +1,11 @@
 import makeListPromos from './list-promos';
 import makeSelectPromo from './select-promo';
 import makeInsertPromo from './add-promo';
+import makeUpdatePromo from './edit-promo';
 
 import promosDB from '../../data-access/promos';
 
 export const listPromos = makeListPromos(promosDB);
 export const selectPromo = makeSelectPromo(promosDB);
 export const insertPromo = makeInsertPromo(promosDB);
+export const updatePromo = makeUpdatePromo(promosDB);
