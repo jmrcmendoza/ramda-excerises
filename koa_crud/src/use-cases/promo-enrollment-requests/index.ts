5c3375c7d9aa0e2e5e7f8d43f0aaa5fe82aa85c6
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-19 09:21:15 +0800

    feat: added approve enrollment reques use case

--- koa_crud/src/use-cases/promo-enrollment-requests/index.ts
+++ koa_crud/src/use-cases/promo-enrollment-requests/index.ts
@@ -1,5 +1,6 @@
 import makeListPromoEnrollmentRequests from './list-promo-enrollment-requests';
 import makeEnrollToPromo from './enroll-to-promo';
+import makeApprovePromoEnrollmentRequest from './approve-promo-enrollment-request';
 
 import promoEnrollmentRequestsDB from '../../data-access/promo-enrollment-requests';
 
@@ -7,3 +8,6 @@ export const listPromoEnrollmentRequests = makeListPromoEnrollmentRequests(
   promoEnrollmentRequestsDB,
 );
 export const enrollToPromo = makeEnrollToPromo(promoEnrollmentRequestsDB);
+export const approvePromoEnrollmentRequest = makeApprovePromoEnrollmentRequest(
+  promoEnrollmentRequestsDB,
+);
