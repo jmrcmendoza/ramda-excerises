a0e62948a63e0e74f05dcf55a95742df7f10e4a2
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-19 09:26:29 +0800

    feat: added process enrollment request use case

--- koa_crud/src/use-cases/promo-enrollment-requests/index.ts
+++ koa_crud/src/use-cases/promo-enrollment-requests/index.ts
@@ -1,5 +1,6 @@
 import makeListPromoEnrollmentRequests from './list-promo-enrollment-requests';
 import makeEnrollToPromo from './enroll-to-promo';
+import makeProcessPromoEnrollmentRequest from './process-promo-enrollment-request';
 import makeApprovePromoEnrollmentRequest from './approve-promo-enrollment-request';
 
 import promoEnrollmentRequestsDB from '../../data-access/promo-enrollment-requests';
@@ -8,6 +9,9 @@ export const listPromoEnrollmentRequests = makeListPromoEnrollmentRequests(
   promoEnrollmentRequestsDB,
 );
 export const enrollToPromo = makeEnrollToPromo(promoEnrollmentRequestsDB);
+export const processPromoEnrollmentRequest = makeProcessPromoEnrollmentRequest(
+  promoEnrollmentRequestsDB,
+);
 export const approvePromoEnrollmentRequest = makeApprovePromoEnrollmentRequest(
   promoEnrollmentRequestsDB,
 );
