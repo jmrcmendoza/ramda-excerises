380080a1fe942c45a3cf199df80a2b3cefa63a4c
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-19 08:51:22 +0800

    feat: added promo enrollment request data access

---
+++ koa_crud/src/data-access/promo-enrollment-requests/index.ts
@@ -0,0 +1,9 @@
+import PromoEnrollmentRequestModel from '../../models/promo-enrollment-requests';
+
+import promoEnrollmentRequestQueries from './promo-enrollment-requests';
+
+const promoEnrollmentRequestsDB = promoEnrollmentRequestQueries({
+  promoEnrollmentRequests: PromoEnrollmentRequestModel,
+});
+
+export default promoEnrollmentRequestsDB;
