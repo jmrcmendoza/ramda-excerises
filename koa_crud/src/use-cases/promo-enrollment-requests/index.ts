c60f0f7f1b61398597008bb361eb8212f018002c
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-19 09:55:14 +0800

    feat: added select one enrollment request use case

--- koa_crud/src/use-cases/promo-enrollment-requests/index.ts
+++ koa_crud/src/use-cases/promo-enrollment-requests/index.ts
@@ -1,4 +1,5 @@
 import makeListPromoEnrollmentRequests from './list-promo-enrollment-requests';
+import makeSelectPromoEnrollmentRequest from './select-promo-enrollment-request';
 import makeEnrollToPromo from './enroll-to-promo';
 import makeProcessPromoEnrollmentRequest from './process-promo-enrollment-request';
 import makeApprovePromoEnrollmentRequest from './approve-promo-enrollment-request';
@@ -7,6 +8,9 @@ import makeRejectPromoEnrollmentRequest from './reject-promo-enrollment-request'
 import promoEnrollmentRequestsDB from '../../data-access/promo-enrollment-requests';
 
 export const listPromoEnrollmentRequests = makeListPromoEnrollmentRequests(
+  promoEnrollmentRequestsDB,
+);
+export const selectOnePromoEnrollmentRequest = makeSelectPromoEnrollmentRequest(
   promoEnrollmentRequestsDB,
 );
 export const enrollToPromo = makeEnrollToPromo(promoEnrollmentRequestsDB);
