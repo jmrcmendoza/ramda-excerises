34165e24d2e4e51b4d4dbf17deed9b384be3a3af
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-22 10:24:03 +0800

    feat: added custom error

--- koa_crud/src/use-cases/promos/delete-promo.ts
+++ koa_crud/src/use-cases/promos/delete-promo.ts
@@ -1,16 +1,24 @@
 import R from 'ramda';
 import { PromoQueries } from '../../data-access/promos/promos';
+import { PromoValidationError } from '../../entities/promos/promo';
+
+class PromoDeleteError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'PROMO_STATUS_ERROR';
+  }
+}
 
 export default function makeDeletePromo(promosDB: PromoQueries) {
   return async function deletePromo(id: string): Promise<any> {
     if (!id) {
-      throw new Error('ID must be provided.');
+      throw new PromoValidationError('ID must be provided.');
     }
 
     const promo = await promosDB.selectOnePromo(id);
 
     if (R.prop('status')(promo) === 'ACTIVE') {
-      throw new Error('Cannot delete active promo.');
+      throw new PromoDeleteError('Cannot delete active promo.');
     }
 
     return promosDB.deletePromo(id);
