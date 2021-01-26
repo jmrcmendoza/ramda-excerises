673381ca5721e40f3f769567c149098532ee2bf0
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-26 10:08:12 +0800

    feat: added query parameters

--- koa_crud/src/use-cases/promos/list-promos.ts
+++ koa_crud/src/use-cases/promos/list-promos.ts
@@ -1,7 +1,10 @@
 import { PromoQueries } from '../../data-access/promos/promos';
 
 export default function makeListPromos(promosDB: PromoQueries) {
-  return async function listPromos(): Promise<any> {
-    return promosDB.listPromos();
+  return async function listPromos(
+    limit: number | null,
+    cursor: string | null,
+  ): Promise<any> {
+    return promosDB.listPromos(limit, cursor);
   };
 }
