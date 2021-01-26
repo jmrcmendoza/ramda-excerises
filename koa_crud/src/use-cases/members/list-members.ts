9b9d81c1cbc1940a1f8e1b1fb3fbd117ca5b0899
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-26 09:55:33 +0800

    feat: added query parameters

--- koa_crud/src/use-cases/members/list-members.ts
+++ koa_crud/src/use-cases/members/list-members.ts
@@ -1,7 +1,10 @@
 import { MemberQueries } from '../../data-access/members/members';
 
 export default function makeListMembers(membersDB: MemberQueries) {
-  return async function listMembers(): Promise<any> {
-    return membersDB.listMembers();
+  return async function listMembers(
+    limit: number | null,
+    cursor: string | null,
+  ): Promise<any> {
+    return membersDB.listMembers(limit, cursor);
   };
 }
