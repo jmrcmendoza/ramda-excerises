43981ad530a53dc28c5b6c85f8f9bbc123fa7032
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-01-27 14:07:35 +0800

    test: updated member controller test
    updated imports

--- koa_crud/test/integration/controllers/members/members.spec.ts
+++ koa_crud/test/integration/controllers/members/members.spec.ts
@@ -4,14 +4,14 @@ import chaiHttp from 'chai-http';
 import chaiAsPromised from 'chai-as-promised';
 import Chance from 'chance';
 import R from 'ramda';
-import server from '../../../../src';
+import server from '@Server';
 import {
   delMember,
   getMembers,
   getOneMember,
   postMember,
   putMember,
-} from '../../../../src/controllers/members';
+} from '@Controllers/members';
 
 const chance = new Chance();
 
