95c6fc7e77bbe3538782eaf47eb886cff444f01c
jmrcmendoza <jmrcmendoza14@gmail.com>
2021-02-01 16:19:11 +0800

    feat: updated promo use case
    added filter argument

--- koa_crud/src/use-cases/promos/list-promos.ts
+++ koa_crud/src/use-cases/promos/list-promos.ts
@@ -4,7 +4,8 @@ export default function makeListPromos(promosDB: PromoQueries) {
   return async function listPromos(
     limit: number | null,
     cursor: string | null,
+    filter: any,
   ): Promise<any> {
-    return promosDB.listPromos(limit, cursor);
+    return promosDB.listPromos(limit, cursor, filter);
   };
 }
